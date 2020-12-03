585b363d53911e04a7d50c31436641e357fe8694
Pablo Daniel Folino <pfolino@gmail.com>
2020-12-03 17:32:50 -0300

    Version 1.1

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 | Versión | Descripción | Cumplido |
 | :-: | :-: | :-: |
-[1] | Programa que se comunica con el ttyUSB1 y hace un loop. Si se presiona una tecla se envía a la PC “>SW:X,Y\r\n” Siendo X el número de salida (0,1, o 2) e Y el nuevo estado a setear (0 o 1), y la PC contesta “>OUT:X,Y\r\n” Siendo X el número de salida (0,1, o 2) e Y el estado (0 o 1). El programa contempla señales SIGINT-SIGTERM.| ✔ |
-[2] | Futura versión | ✔ |
+[1.0] | Programa que se comunica con el ttyUSB1 y hace un loop. Si se presiona una tecla se envía a la PC “>SW:X,Y\r\n” Siendo X el número de salida (0,1, o 2) e Y el nuevo estado a setear (0 o 1), y la PC contesta “>OUT:X,Y\r\n” Siendo X el número de salida (0,1, o 2) e Y el estado (0 o 1). El programa contempla señales SIGINT-SIGTERM.| ✔ |
+[1.1] | Posee un hilo principal que constantemente lee el RS232(cada 100useg), y si recibe un dato lo envía al socket. Se agrega un hilo(start_tcp) para procesar la función accept. Al procesar SIGINT y/o SIGTERM se cierran el puerto serie, el socket, y el hilo segundario. | ✔ |
+[1.2]] | Futura versión |   |
+[1.3]] | Futura versión |   |
--- SerialServiceEsqueleto/main.c
+++ SerialServiceEsqueleto/main.c
@@ -1,47 +1,120 @@
+//======================================= Librerías ===============================================
 #include <stdio.h>
 #include <stdlib.h>
 #include "SerialManager.h"
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
 
-#define SEGUNDO	2
+#include <pthread.h>
+
+//=============================== Definición de constantes ========================================
+#define ESPERA	100000		// Se lee cada 100mseg el puero serie
 // El SERIAL_PORT 0--> implica que se conecta a /dev/ttyUSB0, se especifica en "serial.c"
 #define SERIAL_PORT	1
 #define BAUDRATE	115200
+
+#define PUERTO_TCP	10000
+#define ADDR_IP		"127.0.0.1"
 
 #define MSG_SERIAL_SND ">OUT:1,1\r\n"
 #define MSG_SERIAL_RCV ">SW:X,Y\r\n"
 #define SIZE_MSG_SND strlen(MSG_SERIAL_SND)
 #define SIZE_MSG_RCV strlen(MSG_SERIAL_SND)
 
+//=================================== Variablos globales ==========================================
+static 	pthread_t h_thread,h_thread1;
+void* ret;
+volatile sig_atomic_t flag_fin;
 
+// Variables que se usan para la conexión tcp
+socklen_t addr_len;
+struct sockaddr_in clientaddr;
+struct sockaddr_in serveraddr;
+char buffer_rx[128];
+int newfd;
+int n;
+int fd_s;
+
+//=================================== Manejo de señales ===========================================
 void signal_receive(int signal)
 {
-    write(1,"Capturo las señales...\n",23);
+	write(1,"Capturo las señales...\n",23);
 
+	// Termino el programa de forma controlada
+	flag_fin=1;
+}
 
-   serial_close();
-   exit(0);
+void bloquearSign(void)
+{
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGINT);
+    sigaddset(&set, SIGTERM);
+    pthread_sigmask(SIG_BLOCK, &set, NULL);
+}
+
+void desbloquearSign(void)
+{
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGINT);
+    sigaddset(&set, SIGTERM);
+    pthread_sigmask(SIG_UNBLOCK, &set, NULL);
 }
 
 
 
 
+//=================================== Manejo de hilos ============================================
+void* read_tcp(void* message)
+{
+	// Leemos mensaje de cliente
+	//if( (n = recv(newfd,buffer,128,0)) == -1 )
+	if( (n = read(newfd,buffer_rx,128)) == -1 ){
+			perror("Error leyendo mensaje en socket");
+			exit(1);
+		}
+	buffer_rx[n]=0x00;
+	serial_send(buffer_rx,n);
+	printf("Recibi %d bytes.:%s\n",n,buffer_rx);
+	sleep(ESPERA);
+	return NULL;
+}
 
+void* start_tcp(void* message)
+{
+	//printf ("%s\n", (const char *) message);
 
+	while(1){
+		// Ejecutamos accept() para recibir conexiones entrantes,el newfd es activo
+		// Ojo: la función accept es bloqueante
+		addr_len = sizeof(struct sockaddr_in);
+    	if ( (newfd = accept(fd_s, (struct sockaddr *)&clientaddr, &addr_len)) == -1){
+	 		perror("Error en accept");
+	   		exit(1);
+		}
+	}
+	usleep(ESPERA);
+	return NULL;
+}
 
-
-
-
+//=================================== Programa Principal ==========================================
 int main(void)
 {
 	char data_send[]=MSG_SERIAL_SND;
 	char data_rcvd[]=MSG_SERIAL_RCV;
+	flag_fin=0;
+
 	printf("Inicio Serial Service\r\n");
 
-   /* Configuro las señales */
+   // Configuro las señales 
     struct sigaction sa;
     sa.sa_handler = signal_receive;
     sa.sa_flags =0; 					//SA_RESTART;
@@ -55,20 +128,73 @@ int main(void)
         	exit(1);
         }
 	
+	// Abro el canal serie hacia la EDU-CIAA
 	if(serial_open(SERIAL_PORT,BAUDRATE) != 0){
 		printf("No se pudo abrir el puerto serie\n");
-		return 0;
+		return -1;
 	}	
+
+	// Se configura la conexión tcp como servidor
+	// Creamos socket
+	fd_s = socket(AF_INET,SOCK_STREAM, 0);
+	if(fd_s==-1){
+		printf("No se pudo crear el socket\n");
+		return -1;
+	}
+	// Cargamos datos de IP:PORT del server
+    bzero((char*) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(PUERTO_TCP);
+    //serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    if(inet_pton(AF_INET, ADDR_IP, &(serveraddr.sin_addr))<=0){
+        	fprintf(stderr,"ERROR invalid server IP\r\n");
+        	return 1;
+    }
+	// Abrimos puerto con bind(), asocia la dirección IP al puerto--> IP:PUERTO
+	if (bind(fd_s, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) == -1) {
+		close(fd_s);
+		perror("Error al asociar IP:PUERTO");
+		return 1;
+	}
+	// Seteamos socket en modo Listening, para escuchar conexiones entrantes
+	if (listen (fd_s, 10) == -1) // backlog=10, hasta 10 conexiones entrantes
+  	{
+    	perror("error en listen");
+    	exit(1);
+  	}
+
+
+	// Creación de un hilo para la aceptación de la conexión del TCP
+	bloquearSign();
+	if (pthread_create (&h_thread, NULL, start_tcp,NULL)!=0) {
+		perror("Error al crear el hilo de start_tcp");
+		return -1;
+	}
+	// Asigno modo detached al hilo start_tcp
+	pthread_detach(h_thread);
+	desbloquearSign();
+
+
 
 	while(1){
 		if(serial_receive(data_rcvd,SIZE_MSG_RCV)!=0){
-			data_send[5]=data_rcvd[4];
-			data_send[7]=data_rcvd[6];
-			serial_send(data_send,SIZE_MSG_SND);
-			sleep(SEGUNDO);		
+			printf("Se recibió del puerto serie: %s",data_rcvd);
+			// Enviamos mensaje a cliente
+    		if (write (newfd, data_rcvd, SIZE_MSG_RCV) == -1){
+      			perror("Error escribiendo mensaje en socket");
+      			exit (1);
+    		}
 		}
+		usleep(ESPERA);
+		if(flag_fin==1){
+			printf("\n Termino el programa \n");
+			// Cerramos conexion con cliente
+    		close(newfd);
+			//pthread_join (h_thread, &ret);
+			serial_close();
+			return 0;
+		}	
 	}
-	
 
 	// El programa no debería llegar aquí
 	return 0;
